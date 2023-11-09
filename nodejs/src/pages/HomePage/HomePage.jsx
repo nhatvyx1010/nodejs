c4b34a90d109f31808b14852a1a1cf7a2742c81f
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-09 21:36:38 +0700

    Add files via upload

--- nodejs/src/pages/HomePage/HomePage.jsx
+++ nodejs/src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import TypeProduct from "../../components/TypeProduct/TypeProduct";
 import { WrapperTypeProduct } from "./style";
+import SliderComponent from "../../components/SliderComponent/SliderComponent";
+import slider1 from '../../assets/images/slider1.webp';
+import slider2 from '../../assets/images/slider2.webp';
+import slider3 from '../../assets/images/slider3.webp';
 
 const HomePage = () => {
     const arr = ['TV', 'Tủ lạnh', 'Laptop']
     return (
+    <>
         <div style={{padding: '0 120px'}}>
-            <WrapperTypeProduct>
-                {arr.map((item) => {
-                    return(
-                        <TypeProduct name ={item} key={item} />
-                    )
-                })}
-            </WrapperTypeProduct>
-            HomePage
+        <WrapperTypeProduct>
+            {arr.map((item) => {
+                return(
+                    <TypeProduct name ={item} key={item} />
+                )
+            })}
+        </WrapperTypeProduct>
         </div>
+        <div id="container" style={{backgroundColor: '#efefef', padding: '0 120px'}}>
+            <SliderComponent arrImages={[slider1, slider2, slider3]} />
+        </div>
+    </>
     );
 }
 
