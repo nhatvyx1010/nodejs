0e515230543792c7c8c55ec2705247350996a836
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-09 21:34:13 +0700

    Add files via upload

--- nodejs/src/components/HeaderComponent/style.js
+++ nodejs/src/components/HeaderComponent/style.js
@@ -5,6 +5,8 @@ export const WrapperHeader = styled(Row)`
     padding: 10px 120px;
     background-color: #84D9BA;
     align-items: center;
+    gap: 16px;
+    flex-wrap: nowrap;
 `
 export const WrapperTextHeader = styled.span`
     font-size: 18px;
@@ -23,4 +25,5 @@ export const WrapperHeaderAccount = styled.div`
 export const WrapperTextHeaderSmall = styled.span `
     font-size: 12px;
     color: #fff;
+    white-space: nowrap;
 `
