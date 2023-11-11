4fcc4077f4a8047f5e5c65a7b2b03058a8ea6b79
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-11 23:04:31 +0700

    Add files via upload

---
+++ nodejs/src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,70 @@
+import { Checkbox, Rate } from 'antd'
+import React from 'react'
+import { WrapperContent, WrapperLableText, WrapperTextPrice, WrapperTextValue } from './style'
+
+const NavBarComponent = () => {
+    const onChange = () => { }
+    const renderContent = (type, options) => {
+        switch (type) {
+            case 'text':
+                return options.map((option) => {
+                    return (
+                        <WrapperTextValue>{option}</WrapperTextValue>
+                    )
+                })
+            case 'checkbox':
+                return (
+                    <Checkbox.Group style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '12px'}} onChange={onChange}>
+                        {options.map((option) => {
+                            return (
+                                <Checkbox style={{marginLeft: 0}} value={option.value}>{option.label}</Checkbox>
+                            )
+                        })}
+                    </Checkbox.Group>
+                )
+            case 'star':
+                return options.map((option) => {
+                    console.log('check', option)
+                    return (
+                        <div style={{display: 'flex'}}>
+                            <Rate style={{fontSize: '12px'}} disabled defaultValue={option} />
+                            <span> {`từ ${option} sao`}</span>
+                        </div>
+                    )
+                })
+                case 'price':
+                    return options.map((option) => {
+                        return (
+                            <WrapperTextPrice>
+                                {option}
+                            </WrapperTextPrice>
+                        )
+                    })
+            default:
+                return {}
+        }
+    }
+
+    return (
+        <div>
+            <WrapperLableText>Lable</WrapperLableText>
+            <WrapperContent>
+                {renderContent('text', ['Tủ lạnh', 'TV', 'Máy giặt'])}
+            </WrapperContent>
+            <WrapperContent>
+                {renderContent('checkbox', [
+                        {value: 'a', label: 'A'},
+                        {value: 'b', label: 'B'}
+                    ])}
+            </WrapperContent>
+            <WrapperContent>
+                {renderContent('star', [3, 4, 5])}
+            </WrapperContent>
+            <WrapperContent>
+                {renderContent('price', ['duoi 40', 'tren 50'])}
+            </WrapperContent>
+        </div>
+    )
+}
+
+export default NavBarComponent
