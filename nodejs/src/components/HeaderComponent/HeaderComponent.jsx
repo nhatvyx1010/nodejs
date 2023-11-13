9c026dc6ef7d7b2118f6c49f37e6d258f721274e
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-13 10:58:00 +0700

    Add files via upload

--- nodejs/src/components/HeaderComponent/HeaderComponent.jsx
+++ nodejs/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col} from 'antd'
+import {Badge, Col} from 'antd'
 import { WrapperHeader, WrapperTextHeader, WrapperHeaderAccount, WrapperTextHeaderSmall } from "./style";
 import ButtonInputSearch from "../ButtonInputSearch/ButtonInputSearch";
 import {UserOutlined, CaretDownOutlined, ShoppingCartOutlined} from '@ant-design/icons'
@@ -32,7 +32,9 @@ const HeaderComponent = () => {
                         </div>
                     </WrapperHeaderAccount>
                     <div>
-                        <ShoppingCartOutlined style={{fontSize: '30px', color:'#fff'}} />
+                        <Badge count={4} size="small">
+                            <ShoppingCartOutlined style={{fontSize: '30px', color:'#fff'}} />
+                        </Badge>
                         <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
                     </div>
                 </Col>
