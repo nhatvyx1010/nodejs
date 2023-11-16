e395a0c6e1b060fe7d5dd5cd36d7c871e530ee4e
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-16 08:37:21 +0700

    Add files via upload

--- nodejs/src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ nodejs/src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import ProductDetailComponent from "../../components/ProductDetailComponent/ProductDetailComponent";
 
 const ProductDetailPage = () => {
     return (
-        <div>ProductDetailPage</div>
+        <div style={{ padding: '0 120px', background: '#efefef', height: '1000px'}}>
+            <h3>Trang chủ</h3>
+            <ProductDetailComponent />
+        </div>
     );
 }
 
