a69d1e6943e64e251718baf58e1263a848051064
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-16 08:36:47 +0700

    Add files via upload

--- nodejs/src/pages/HomePage/HomePage.jsx
+++ nodejs/src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,6 @@ import slider1 from '../../assets/images/slider1.webp';
 import slider2 from '../../assets/images/slider2.webp';
 import slider3 from '../../assets/images/slider3.webp';
 import CardComponent from "../../components/CardComponent/CardComponent";
-import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
-import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
 
 const HomePage = () => {
     const arr = ['TV', 'Tủ lạnh', 'Laptop']
