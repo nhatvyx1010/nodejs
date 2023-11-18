78670194b250f753e07e75ff8142d07b06cd6013
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-18 21:17:18 +0700

    Add files via upload

--- nodejs/src/components/CardComponent/CardComponent.jsx
+++ nodejs/src/components/CardComponent/CardComponent.jsx
@@ -12,16 +12,26 @@ const CardComponent = () => {
             bodyStyle={{ padding: '10px'}}
             cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
         >
-        <WrapperImageStyle src={logo} alt="logo" />
+        <img
+            src= {logo}
+            style={{
+                width: '68px',
+                height: '14px',
+                position: 'absolute',
+                top: -1, 
+                left: -1,
+                borderTopLeftRadius: '3px'
+            }}
+        />
         <StyleNameProduct>Iphone</StyleNameProduct>
         <WrapperReportText>
             <span style={{ marginRight: '4px'}}>
-                <span>4.96</span> <StarFilled style={{fontSize: '12px', color: 'rgb(253, 216, 54)'}} />
+                <span>4.96 </span> <StarFilled style={{fontSize: '12px', color: 'rgb(253, 216, 54)'}} />
             </span>
             <WrapperStyleTextSell> | Da ban 1000+</WrapperStyleTextSell>
         </WrapperReportText>
         <WrapperPriceText>
-            1.000.000d 
+            <span style={{marginRight: '8px'}}>1.000.000</span>
             <WrapperDiscountText>
                 -5%
             </WrapperDiscountText>
