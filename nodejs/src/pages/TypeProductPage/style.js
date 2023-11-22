a8b0defe499e7f945765ab3e3a05f33fad854420
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-22 20:38:33 +0700

    Add files via upload

--- nodejs/src/pages/TypeProductPage/style.js
+++ nodejs/src/pages/TypeProductPage/style.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 
 export const WrapperProducts = styled.div`
     display: flex;
-    justify-content: center;
-    gap: 15px;
+    gap: 12px;
     margin-top: 20px;
     flex-wrap: wrap;
 `
@@ -13,7 +12,8 @@ export const WrapperNavbar = styled(Col)`
     background: #fff;
     margin-right: 10px;
     padding: 10px;
-    border-radius: 3px;
+    border-radius: 4px;
     height: fit-content;
     margin-top: 20px; 
+    width: 200px;
 `
