1b7531efa648006c9c23d748ca5041d0dca8233b
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-11-22 20:37:55 +0700

    Add files via upload

--- nodejs/src/pages/SignInPage/SignInPage.jsx
+++ nodejs/src/pages/SignInPage/SignInPage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { WrapperContainerLeft, WrapperContainerRight, WrapperTextLight } from "./style";
 import InputForm from "../../components/InputForm/InputForm";
 import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import imageLogo from '../../assets/images/logo-login.png'
 import { Divider, Image } from "antd";
+import {EyeFilled, EyeInvisibleFilled} from '@ant-design/icons'
 
 const SignInPage = () => {
+    const [isShowPassword, setIsShowPassword] = useState(false)
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'rgb(0, 0, 0, 0.53)', height: '100vh'}}>
             <div style={{width: '800px', height: '445px', borderRadius: '6px', background: '#fff', display:'flex'}}>
@@ -13,7 +15,24 @@ const SignInPage = () => {
                 <h1>Xin chào</h1> 
                 <p>Đăng nhập và tạo tài khoản</p>
                 <InputForm style={{marginBottom: '10px'}} placeholder="abc@gmail.com"/>
-                <InputForm placeholder="password" />
+                <div style={{position: 'relative'}}>
+                    <span
+                        style={{
+                            zIndex: 10,
+                            position: 'absolute',
+                            top: '4px',
+                            right: ' 8px'
+                        }}
+                    >{
+                        isShowPassword ? (
+                            <EyeFilled />
+                        ) : (
+                            <EyeInvisibleFilled />
+                        )
+                    }
+                    </span>
+                    <InputForm placeholder="password" type={isShowPassword ? "text" : "password"} />
+                </div>
                 <ButtonComponent bordered={false} size={40} styleButton={{background: 'rgb(255, 57, 69)', height: '48px', width: '100%', border: 'none', borderRadius: '4px', margin: '26px 0 10px'}} 
                         textButton={'Đăng nhập'}
                         styleTextButton={{color: '#fff', fontSize: '15px', fontWeight: '300'}}>
--- nodejs/src/pages/SignUpPage/SignUpPage.jsx
+++ nodejs/src/pages/SignUpPage/SignUpPage.jsx
@@ -4,8 +4,12 @@ import InputForm from "../../components/InputForm/InputForm";
 import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import imageLogo from '../../assets/images/logo-login.png'
 import { Image } from "antd";
+import {EyeFilled, EyeInvisibleFilled} from '@ant-design/icons'
+import {useState} from 'react'
 
 const SignUpPage = () => {
+    const [isShowPassword, setIsShowPassword] = useState(false)
+    const [isShowConfirmPassword, setIsShowComfirmPassword] = useState(false)
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'rgb(0, 0, 0, 0.53)', height: '100vh'}}>
             <div style={{width: '800px', height: '445px', borderRadius: '6px', background: '#fff', display:'flex'}}>
@@ -13,8 +17,42 @@ const SignUpPage = () => {
                 <h1>Xin chào</h1> 
                 <p>Đăng nhập và tạo tài khoản</p>
                 <InputForm style={{marginBottom: '10px'}} placeholder="abc@gmail.com"/>
-                <InputForm placeholder="password" style={{marginBottom: '10px'}} />
-                <InputForm placeholder="confirm password" />
+                <div style={{position: 'relative'}}>
+                    <span
+                        style={{
+                            zIndex: 10, 
+                            position: 'absolute',
+                            top: '4px',
+                            right: '8px'
+                        }}
+                    >{
+                        isShowConfirmPassword ? (
+                            <EyeFilled />
+                        ) : (
+                            <EyeInvisibleFilled />
+                        )
+                    }
+                    </span>
+                    <InputForm placeholder="password" style={{marginBottom: '10px'}} />
+                </div>
+                <div style={{position: 'relative'}}>
+                    <span
+                        style={{
+                            zIndex: 10, 
+                            position: 'absolute',
+                            top: '4px',
+                            right: '8px'
+                        }}
+                    >{
+                        isShowConfirmPassword ? (
+                            <EyeFilled />
+                        ) : (
+                            <EyeInvisibleFilled />
+                        )
+                    }
+                    </span>
+                    <InputForm placeholder="confirm password" />
+                </div>
                 <ButtonComponent bordered={false} size={40} styleButton={{background: 'rgb(255, 57, 69)', height: '48px', width: '100%', border: 'none', borderRadius: '4px', margin: '26px 0 10px'}} 
                         textButton={'Đăng nhập'}
                         styleTextButton={{color: '#fff', fontSize: '15px', fontWeight: '300'}}>
