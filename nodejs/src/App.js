e0d0950685a9f10312fcbee09c5e34d8e37d9459
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-12-11 21:02:31 +0700

    Add files via upload

--- nodejs/src/App.js
+++ nodejs/src/App.js
@@ -1,10 +1,7 @@
-import axios from 'axios'
 import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { routes } from './routes'
-import { useEffect } from 'react'
 import DefaultComponent from './components/DefaultComponent/DefaultComponent'
-import { useQuery } from '@tanstack/react-query'
 
 function App() {
 
@@ -12,16 +9,16 @@ function App() {
   // useEffect(() => {
   //   fetchApi()
   // }, [])
-  const fetchApi = async () => {
-    try {
-      const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`);
-      return res.data
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-    }
-  };
-  const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi})
-  console.log('query', query)
+  // const fetchApi = async () => {
+  //   try {
+  //     const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`);
+  //     return res.data
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error.message);
+  //   }
+  // };
+  // const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi})
+  // console.log('query', query)
   return (
     <div>
       <Router>
