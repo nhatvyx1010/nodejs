3da49d141b13d7287ff94247d71f01385dff61d8
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2023-12-16 22:21:03 +0700

    Add files via upload

--- nodejs/src/App.js
+++ nodejs/src/App.js
@@ -1,24 +1,53 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { routes } from './routes'
 import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import { isJsonString } from './untils'
+import jwt_decode, { jwtDecode } from 'jwt-decode';
+import * as UserService from './services/UserService'
+import { useDispatch } from 'react-redux'
+import { updateUser } from './redux/slices/userSlice'
+import axios from 'axios'
 
 function App() {
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    const {storageData, decoded} = handleDecoded()
 
-  // useEffect(() => {
-  //   fetchApi()
-  // }, [])
-  // const fetchApi = async () => {
-  //   try {
-  //     const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`);
-  //     return res.data
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error.message);
-  //   }
-  // };
-  // const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi})
-  // console.log('query', query)
+    if(decoded?.id){
+        handleGetDetailsUser(decoded?.id, storageData)
+    }
+  }, [])
+
+  const handleDecoded = () => {
+    let storageData = localStorage.getItem('access_token')
+    let decoded = {}
+
+    if(storageData && isJsonString(storageData)) {
+      storageData = JSON.parse(storageData)
+      decoded = jwtDecode(storageData)
+    }
+    return {decoded, storageData}
+  }
+
+  UserService.axiosJWT.interceptors.request.use(async (config) => {
+    const currentTime = new Date()
+    const { decoded} = handleDecoded()
+    if (decoded?.exp < currentTime.getTime() / 1000){
+      const data = await UserService.refreshToken()
+      config.headers['token'] = `Bearer ${data?.access_token}`
+    }
+    return config
+  }, (err) => {
+    return Promise.reject(err)
+  });
+
+  const handleGetDetailsUser = async (id, token) => {
+    const res = await UserService.getDetailUser(id, token)
+    dispatch(updateUser({...res?.data, access_token: token}))
+}
+
   return (
     <div>
       <Router>
